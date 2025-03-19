d74a0374debf1c2c2c5894d05e1f9cf52e0c82a7
manuo1 <manuo1@free.fr>
2025-03-19 20:52:36 +0100

    Fix: Add modal close functionality (#1)
    - Add event listener to close button
    - Create closeModal() function
    - Add event listener to close modal when clicking outside
    
    Closes #1

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +21,18 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close modal event
+closeBtn.addEventListener("click", closeModal);
 
+
+// close modal form with X
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+// close modal when clicking outside of the modal content
+window.addEventListener("click", function(event) {
+  if (event.target == modalbg) {
+    closeModal();
+  }
+});
